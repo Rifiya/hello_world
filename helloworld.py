060bdaef7abc82ca53204028e8e52d61e88f08a2
Rifiya <57045022+Rifiya@users.noreply.github.com>
2020-01-21 08:54:53 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#hii
+greeting = "hello"
+#message = message.replace('world','universe')
+name = "Bob"
+message1 = greeting + name
+print(message1)
+message = '{}, {}.Welcome!'.format(greeting, name.upper())
+print(dir(name))
+print(message)
+
+
